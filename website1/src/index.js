2c1fc4fc041f78b8ee34135ed0b7f2fffe22bc76
Nemo-Git-Hub <77303558+Nemo-Git-Hub@users.noreply.github.com>
2022-10-10 23:35:07 +0300

    feat: add modal & upgrade variables (#15)
    Co-authored-by: Kolesnyk Oleh <ovk1@ukr.net>

--- website1/src/index.js
+++ website1/src/index.js
@@ -1,1 +1,24 @@
 import './scss/main.scss'
+
+const modal = document.querySelector(".modal");
+const modalOverlay = document.querySelector(".modal__overlay");
+const triggers = document.querySelectorAll(".trigger-modal");
+const closeButton = document.querySelector(".modal__close-button");
+const body = document.querySelector("body");
+
+function toggleModal() {
+	modal.classList.toggle("show-modal");
+	body.classList.toggle("lock");
+}
+
+function windowOnClick(event) {
+	if (event.target === modalOverlay) {
+		toggleModal();
+	}
+}
+
+triggers.forEach((trigger) => {
+	trigger.addEventListener("click", toggleModal);
+})
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
