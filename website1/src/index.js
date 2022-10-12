602af14a18ba57ebcf9264d83edfcc1df8276e23
Nemo-Git-Hub <77303558+Nemo-Git-Hub@users.noreply.github.com>
2022-10-12 16:49:26 +0300

    feat: create adaptive header (#16)
    * feat: create adaptive header
    
    * fix to bug adaptive header & prevent flickering on screen Modal window before rendering
    
    * fix to bug
    
    Co-authored-by: Kolesnyk Oleh <ovk1@ukr.net>

--- website1/src/index.js
+++ website1/src/index.js
@@ -1,14 +1,14 @@
 import './scss/main.scss'
 
+// Modal window
 const modal = document.querySelector(".modal");
 const modalOverlay = document.querySelector(".modal__overlay");
 const triggers = document.querySelectorAll(".trigger-modal");
 const closeButton = document.querySelector(".modal__close-button");
-const body = document.querySelector("body");
 
 function toggleModal() {
 	modal.classList.toggle("show-modal");
-	body.classList.toggle("lock");
+	document.body.classList.toggle("lock");
 }
 
 function windowOnClick(event) {
@@ -22,3 +22,47 @@ triggers.forEach((trigger) => {
 })
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+
+// Smooth Scrolling - плавная прокрутка
+
+const menuLinks = document.querySelectorAll('.header-menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	
+	function onMenuLinkClick(event) {
+		const menuLink = event.target
+		const gotoBlock = document.querySelector(menuLink.dataset.goto);
+		
+		
+		if (menuLink.dataset.goto && gotoBlock) {
+			event.preventDefault();
+			
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+			
+			if (iconMenu.classList.contains('active')) {
+				document.body.classList.remove("lock");
+				iconMenu.classList.remove("active");
+				menuBody.classList.remove("active");
+			}
+			
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			
+		}
+	}
+}
+
+// Menu burger
+
+const iconMenu = document.querySelector('.header-nav__menu-icon');
+const menuBody = document.querySelector('.header-nav__body');
+iconMenu?.addEventListener("click", () => {
+	document.body.classList.toggle("lock");
+	iconMenu.classList.toggle("active");
+	menuBody.classList.toggle("active");
+})
